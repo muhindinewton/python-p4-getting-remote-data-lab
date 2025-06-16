d9a1699be88637f37644d59e7171f051e6af451c
Newton Muhindi <developermuhindi@gmail.com>
2025-06-16 17:41:20 +0300

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return requests.get(self.url).json()
+    
+
